e08324dd574a1d20c3bf9cb5404178f4885f5aa8
leriomaggio <valerio.maggio@gmail.com>
2020-11-12 18:46:26 +0000

    Bump version: 1.1.2-pre0 → 1.1.2

--- notexbook/__init__.py
+++ notexbook/__init__.py
@@ -9,7 +9,7 @@
 # License: Apache License 2.0
 # ------------------------------------------------------------------------
 
-__version__ = "1.1.2-pre0"
+__version__ = "1.1.2"
 
 from notexbook.magic import *
 
