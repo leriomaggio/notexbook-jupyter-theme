7660051d876496264e36b33828b6c104853e3324
leriomaggio <valerio.maggio@gmail.com>
2020-12-01 20:04:47 +0000

    noTeXbook CSS aligned w/ v1.1.5-theme

--- custom-css/custom/custom.js
+++ custom-css/custom/custom.js
@@ -13,16 +13,21 @@ http://github.com/leriomaggio
 
 function customise_anchor_links_click() {
     $("a").on("click", function (e) {
-        // Check if header toolbar is visible in the first place
-        if (this.hash !== "") {
+        const hash = this.hash;  // full hash reference, if any (e.g. #numpy.arange)
+        if (hash !== "") {  // check that this is an anchor link
             // Remove any existing handler
             $(this).unbind();
-            // Store hash
-            const hash = this.hash;
-            let t = $(hash);
+            // Get anchor (get rid of sharp symbol)
+            // and escape any invalid character for a JQuery selector
+            // e.g. numpy.arange ==> "numpy\.arange"
+            const anchor = $.escapeSelector(hash.substr(1));
+            // Look for ID selector
+            let anchor_sel = "#"+anchor;
+            let t = $(anchor_sel);
             if ((t === undefined) || (t.length === 0)) {
-                let anchor = hash.substr(1);
-                t = $("a[name='" + anchor + "'");
+                // if not ID, look for a link name anchor
+                anchor_sel = "a[name='" + anchor + "'";
+                t = $(anchor_sel);
             }
             if ((t !== undefined) && (t.length > 0)) {
                 // Prevent default anchor click behavior
@@ -32,7 +37,7 @@ function customise_anchor_links_click() {
                 // The optional number (800) specifies the number of milliseconds
                 // it takes to scroll to the specified area
                 let header_height = $("#header").height();
-                let offset = $(t).offset().top - header_height;
+                let scrollTopOffset = $(t).offset().top - header_height;
 
                 /*
                  * FIX Scrolling Animation Freeze:
@@ -42,7 +47,7 @@ function customise_anchor_links_click() {
                     page.stop(true);  // clearQueue
                 });
                 page.stop(true).animate({
-                    scrollTop: offset
+                    scrollTop: scrollTopOffset
                 }, 800, function () {
                     page.off("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove");
                 });
