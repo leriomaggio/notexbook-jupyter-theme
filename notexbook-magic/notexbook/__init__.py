a753015ab7214da0b7849ab156d087770979f21b
leriomaggio <valerio.maggio@gmail.com>
2020-12-01 20:03:56 +0000

    noTeXbook magic aligned w/ v1.1.5

--- notexbook-magic/notexbook/__init__.py
+++ notexbook-magic/notexbook/__init__.py
@@ -9,7 +9,7 @@
 # License: Apache License 2.0
 # ------------------------------------------------------------------------
 
-__version__ = "1.1.2"
+__version__ = "1.1.5"
 
 from notexbook.magic import *
 
