d3ed6700bdf4d01f931d99aff0e324ba6d16ff49
leriomaggio <valerio.maggio@gmail.com>
2021-04-01 18:48:28 +0100

    DEV - no Jupyter var overload

--- notexbook/__init__.py
+++ notexbook/__init__.py
@@ -9,7 +9,7 @@
 # License: Apache License 2.0
 # ------------------------------------------------------------------------
 
-__version__ = "2.0.0-pre0"
+__version__ = "2.0.0-pre1"
 
 from notexbook.magic import *
 
