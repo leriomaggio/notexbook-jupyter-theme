e14e9c80f6451ab539b73803bd69b22d90157022
leriomaggio <valerio.maggio@gmail.com>
2021-04-08 19:44:19 +0100

    Bump version: 2.0.0 → 2.0.1

--- notexbook/__init__.py
+++ notexbook/__init__.py
@@ -9,7 +9,7 @@
 # License: Apache License 2.0
 # ------------------------------------------------------------------------
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 
 from notexbook.magic import *
 
